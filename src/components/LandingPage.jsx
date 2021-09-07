fa343576afd3d50399371629e5faa391909c2899
Mackenzie <youzai0427@gmail.com>
2021-09-07 23:51:02 +0800

    1.Delete import firebase 2. refactor isSignInchecker 3. refactor GoogleSignIn 4. Add legend tag to Provider-SIgnIn class

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,22 +1,15 @@
-import React, { useState } from "react";
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import NoteImage from "../Image_Source/Note.jpg";
+import React from "react";
+import { isSignInChecker } from "./FireBaseApi";
 
 import { GoogleSignIn } from "./FireBaseApi";
 
 export default function LandingPage({ isLogin, setisLogin }) {
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      console.log("user login now change state");
-      setisLogin(true);
-    } else {
-      console.log("user not login");
-    }
-  });
+  const setisLoginState = setisLogin;
+  isSignInChecker(setisLoginState);
+
   const LoginHandle = (e) => {
     e.preventDefault();
-    GoogleSignIn();
+    GoogleSignIn(setisLoginState);
   };
 
   return (
@@ -26,16 +19,18 @@ export default function LandingPage({ isLogin, setisLogin }) {
           <p>
             Your Notes<br></br>You decise
           </p>
-          <img src={NoteImage} alt="Note" />
         </div>
         <form action="submit">
+          <h1>Login</h1>
           <input type="text" placeholder="User" />
           <input type="text" placeholder="Password" />
           <section className="Provider-SignIn">
-            Other Ways:
-            <button onClick={LoginHandle}>
-              <i className="fab fa-google"></i>
-            </button>
+            <fieldset>
+              <legend>or</legend>
+              <button onClick={LoginHandle}>
+                <i className="fab fa-google"></i>
+              </button>
+            </fieldset>
           </section>
         </form>
       </div>
