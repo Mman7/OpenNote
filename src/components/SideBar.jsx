641efc235ca2eff8b08018b16a4b86cc0bfb313c
Mackenzie <youzai0427@gmail.com>
2021-09-07 23:51:47 +0800

    rebuild the old code to readable code

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,19 +1,25 @@
-import React, { useContext } from "react";
+import React from "react";
 
 import { SignOut, getCurrentUser } from "./FireBaseApi";
 
 export default function SideBar({ Sidebar, setisLogin }) {
   const SignOutHandle = () => {
-    SignOut();
-    setisLogin(false);
+    const isLoginState = setisLogin;
+    SignOut(isLoginState);
   };
 
   return (
     <div className={`Sidebar ${Sidebar ? "SideBarDisplay" : ""}`}>
-      <button>
-        <img src={getCurrentUser()} alt="" />
-      </button>
-
+      <div className="User-Info">
+        <a>
+          <img src={getCurrentUser().photoURL} alt="User Img" />
+        </a>
+        <h1 className="user-display-name">{getCurrentUser().displayName}</h1>
+        <h1 className="user-email">{getCurrentUser().email}</h1>
+      </div>
+      <button className="hey">hey2</button>
+      <button>hey3</button>
+      <button>hey4</button>
       <button className="SignOut" onClick={SignOutHandle}>
         SignOut
       </button>
