dd150887a367c511d5de0b1f5644fa47d9dfc996
Mackenzie <youzai0427@gmail.com>
2021-09-14 22:44:17 +0800

    Create(new file) Note context + get Note

---
+++ src/components/NoteListContextProvider.jsx
@@ -0,0 +1,19 @@
+import React, { useState, createContext, useEffect } from "react";
+import { getData_From_DataBase } from "./FireBaseApi";
+
+export const NoteContext = createContext([]);
+
+export function NoteListContextProvider({ children }) {
+  const [Notes, setNotes] = useState([]);
+
+  useEffect(async () => {
+    const data = await getData_From_DataBase();
+    setNotes(() => data);
+  }, []);
+
+  return (
+    <NoteContext.Provider value={[Notes, setNotes]}>
+      {children}
+    </NoteContext.Provider>
+  );
+}
