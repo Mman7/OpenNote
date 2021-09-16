7d3d435f397381b253d5e8dcccc3599aa7489bb3
Mackenzie <youzai0427@gmail.com>
2021-09-16 14:30:07 +0800

    Update 🆕 Dynamic update click on note

--- src/components/EditPage/NoteView.jsx
+++ src/components/EditPage/NoteView.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { NoteContext } from "../NoteListContextProvider";
+import { EditNoteContext } from "../EditNoteContextProvider";
+import { NormalizeTime } from "../NormalizeTime";
 
 export default function NoteView() {
+  const [Notes, setNotes] = useContext(NoteContext);
+  const [CurrentEditNote, setCurrentEditNote] = useContext(EditNoteContext);
+  const [Note, setNote] = useState({});
+
+  useEffect(() => {
+    const Edit = Notes.filter((note) => note.title === CurrentEditNote);
+    setNote(Edit[0]);
+  }, [CurrentEditNote]);
+
+  const ThisNote = () => (Note !== undefined ? Note : "");
+
   return (
     <div className="Edit-NoteView">
-      <input className="Edit-title" defaultValue="Title"></input>
-      <input className="Edit-Date" defaultValue="Date"></input>
+      <input className="Edit-title" defaultValue={ThisNote().title}></input>
+      <input
+        className="Edit-Date"
+        defaultValue={NormalizeTime(ThisNote().createdAt)}
+      ></input>
       <textarea
-        defaultValue="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, aut"
         className="Edit-Note-Section"
+        defaultValue={ThisNote().paragraph}
       ></textarea>
     </div>
   );
