7bde4dad6a90e1979c6112b8cc2087ed0cf3e979
Mackenzie <youzai0427@gmail.com>
2021-09-25 17:54:06 +0800

    Fix 🛠 code is more readable

--- src/components/Noteitem.jsx
+++ src/components/Noteitem.jsx
@@ -6,27 +6,30 @@ import { NormalizeTime } from "./NormalizeTime";
 import ReactMarkdown from "react-markdown";
 
 export default function Noteitem({ note }) {
-  const [ShowEdit, setShowEdit] = useContext(ShowEditContext);
   const [Notes, setNotes] = useContext(NoteContext);
+  const [ShowEditPage, setShowEditPage] = useContext(ShowEditContext);
   const [CurrentEditNote, setCurrentEditNote] = useContext(EditNoteContext);
 
   const EditNoteHandle = () => {
-    setShowEdit((prev) => !prev);
+    setShowEditPage((prev) => !prev);
   };
 
   const EditThisNote = (e) => {
-    const EditNoteTitel = e.target.firstChild.innerText;
-    setCurrentEditNote(EditNoteTitel);
+    const NoteId = e.target.dataset.id;
+    const EditThisNote = Notes.filter((note) => note.noteid === NoteId);
+    setCurrentEditNote(EditThisNote[0]);
     EditNoteHandle();
   };
+
   return (
     <div
+      data-id={`${note.noteid}`}
       onClick={(e) => EditThisNote(e)}
       title="Click to edit"
       className="Noteitem"
     >
       <h1 className="Noteitem-title">{note.title}</h1>
-      <h2 className="Noteitem-time">
+      <h2 data-plaintext="true" className="Noteitem-time">
         Lastest Update: {NormalizeTime(note.createdAt)}
       </h2>
       <ReactMarkdown className="Noteitem-paragraph">
