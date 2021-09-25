f7517d8e1ab0655933ad4d0a669a3df66c5f2707
Mackenzie <youzai0427@gmail.com>
2021-09-25 17:56:45 +0800

    Update🔼noteitem key use noteid now

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,13 +1,14 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { NoteContext } from "./NoteListContextProvider";
 import Noteitem from "./Noteitem";
 
 export default function NoteList() {
   const [Notes, setNotes] = useContext(NoteContext);
+
   return (
     <div className="NoteList">
       {Notes.map((note) => (
-        <Noteitem key={note.title} note={note} />
+        <Noteitem key={note.noteid} note={note} />
       ))}
     </div>
   );
