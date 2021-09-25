b8ecd55e126b8f03a7c9a9f066411c4c77cc6339
Mackenzie <youzai0427@gmail.com>
2021-09-25 18:05:06 +0800

    Update🔼Use Draftjs Editor now, no longer react-markdown

--- src/components/EditPage/NoteView.jsx
+++ src/components/EditPage/NoteView.jsx
@@ -2,31 +2,24 @@ import React, { useState, useContext, useEffect } from "react";
 import { NoteContext } from "../NoteListContextProvider";
 import { EditNoteContext } from "../EditNoteContextProvider";
 import { NormalizeTime } from "../NormalizeTime";
-import ReactMarkdown from "react-markdown";
+import MyEditor from "./TextEditor";
 
 export default function NoteView() {
   const [Notes, setNotes] = useContext(NoteContext);
   const [CurrentEditNote, setCurrentEditNote] = useContext(EditNoteContext);
-  const [Note, setNote] = useState({});
-
-  useEffect(() => {
-    const Edit = Notes.filter((note) => note.title === CurrentEditNote);
-    setNote(Edit[0]);
-  }, [CurrentEditNote]);
-
-  const ThisNote = () => (Note !== undefined ? Note : "");
 
   return (
     <div className="Edit-NoteView">
-      <input className="Edit-title" defaultValue={ThisNote().title}></input>
+      <input
+        className="Edit-title"
+        defaultValue={CurrentEditNote?.title}
+      ></input>
       <input
         className="Edit-Date"
-        defaultValue={NormalizeTime(ThisNote().createdAt)}
+        defaultValue={NormalizeTime(CurrentEditNote?.createdAt)}
       ></input>
-      <ReactMarkdown
-        className="Edit-Note-Section"
-        children={ThisNote().paragraph}
-      ></ReactMarkdown>
+      {/* if got note actived code below */}
+      {CurrentEditNote ? <MyEditor CurrentEditNote={CurrentEditNote} /> : ""}
     </div>
   );
 }
