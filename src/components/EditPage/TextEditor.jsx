76ea804ed50ac186a42da470fee316249f6cac22
Mackenzie <youzai0427@gmail.com>
2021-09-27 18:53:29 +0800

    Update🔼remove draftjs

--- src/components/EditPage/TextEditor.jsx
+++ src/components/EditPage/TextEditor.jsx
@@ -1,22 +1,10 @@
-import React, { useEffect } from "react";
-import { Editor, EditorState, ContentState } from "draft-js";
-import "draft-js/dist/Draft.css";
+import React, { useState } from "react";
+import Editor from "./Editor";
 
 export default function MyEditor({ CurrentEditNote }) {
-  //TODO: Fix the Note only Change once
-  //*: Cant use useEffect idk why
-  //* to future me "help"
-  const [editorState, setEditorState] = React.useState(() =>
-    EditorState.createWithContent(
-      ContentState.createFromText(CurrentEditNote.paragraph)
-    )
-  );
-
   return (
-    <Editor
-      placeholder="Enter some text"
-      editorState={editorState}
-      onChange={setEditorState}
-    />
+    <div id="editor">
+      <Editor CurrentEditNote={CurrentEditNote} />
+    </div>
   );
 }
