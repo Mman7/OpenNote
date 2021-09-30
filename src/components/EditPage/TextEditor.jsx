223771ba67c351ebe15f850860e23ba703b5662b
Mackenzie <youzai0427@gmail.com>
2021-09-30 17:02:34 +0800

    Update 🔼 react quill editor

--- src/components/EditPage/TextEditor.jsx
+++ src/components/EditPage/TextEditor.jsx
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
-import Editor from "./Editor";
+import React, { useState, useRef, createRef, useEffect } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+let mainRef = createRef();
 
 export default function MyEditor({ CurrentEditNote }) {
+  // I dont know how to use useRef
+  //TODO: use mainRef.current.unprivilegedEditor.getContents() to get current editor delta content
+
+  //* Next 1. Edit Data 2.Save Data 3.Get Data
+  const [value, setValue] = useState("");
+  useEffect(() => {
+    console.log(mainRef.current.unprivilegedEditor.getContents());
+  }, [value]);
   return (
-    <div id="editor">
-      <Editor CurrentEditNote={CurrentEditNote} />
+    <div className="Edit-Note-Section">
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={setValue}
+        ref={mainRef}
+      />
     </div>
   );
 }
