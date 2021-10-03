a4a761fbab53608cdd8a7227e6b190f62dfad5c7
Mackenzie <youzai0427@gmail.com>
2021-10-04 00:14:21 +0800

    FIX🔧note title and date tracking update

--- src/components/EditPage/NoteView.jsx
+++ src/components/EditPage/NoteView.jsx
@@ -8,16 +8,17 @@ export default function NoteView() {
   const [Notes, setNotes] = useContext(NoteContext);
   const [CurrentEditNote, setCurrentEditNote] = useContext(EditNoteContext);
 
+  useEffect(() => {
+    const noteTitle = document.querySelector(".Edit-title");
+    const noteDate = document.querySelector(".Edit-Date");
+    noteTitle.value = CurrentEditNote?.title;
+    noteDate.value = NormalizeTime(CurrentEditNote?.createdAt);
+  }, [CurrentEditNote]);
+
   return (
     <div className="Edit-NoteView">
-      <input
-        className="Edit-title"
-        defaultValue={CurrentEditNote?.title}
-      ></input>
-      <input
-        className="Edit-Date"
-        defaultValue={NormalizeTime(CurrentEditNote?.createdAt)}
-      ></input>
+      <input className="Edit-title"></input>
+      <input className="Edit-Date"></input>
       <TextEditor CurrentEditNote={CurrentEditNote} />
     </div>
   );
