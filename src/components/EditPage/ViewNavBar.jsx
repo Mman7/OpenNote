4ba06747b22e51c975d4cd41b3cd9fbc81da02ab
Mackenzie <youzai0427@gmail.com>
2021-10-04 01:15:37 +0800

    Add➕ isDifferent Context and Logical Handle

--- src/components/EditPage/ViewNavBar.jsx
+++ src/components/EditPage/ViewNavBar.jsx
@@ -1,18 +1,23 @@
 import React, { useContext } from "react";
 import { ShowEditContext } from "../OpenNote";
+import { isDifferentContext } from "../Context/NoteIsDifferentProvider";
 
 export default function ViewNavBar() {
   const [, setShowEdit] = useContext(ShowEditContext);
+  const [isDifferent, setisDifferent] = useContext(isDifferentContext);
 
   const Back = () => {
     setShowEdit((prev) => !prev);
   };
+
+  const isDifferentHandle = () => (isDifferent ? "Save-Note-btn-show" : "");
 
   return (
     <div className="ViewNavBar">
       <button onClick={Back}>
         <i className="fas fa-chevron-left"></i>
       </button>
+      <button className={`Save-Note-btn ${isDifferentHandle()}`}>✅</button>
     </div>
   );
 }
