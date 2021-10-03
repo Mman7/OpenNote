3721fd1317ea9c72fc20963b1de97e5c516de477
Mackenzie <youzai0427@gmail.com>
2021-10-04 01:16:56 +0800

    import ➕ EditNoteContext + isDIfferentContext

--- src/components/EditPage/TextEditor.jsx
+++ src/components/EditPage/TextEditor.jsx
@@ -1,24 +1,28 @@
-import React, { useState, useRef, createRef, useEffect } from "react";
+import React, { useState, createRef, useEffect, useContext } from "react";
+import { isDifferentContext } from "../Context/NoteIsDifferentProvider";
 import ReactQuill from "react-quill";
+import { EditNoteContext } from "../Context/EditNoteContextProvider";
 import "react-quill/dist/quill.snow.css";
 
 let mainRef = createRef();
 
-export default function MyEditor({ CurrentEditNote }) {
-  // I dont know how to use useRef
-  //TODO: use mainRef.current.unprivilegedEditor.getContents() to get current editor delta content
-
-  //* Next 1. Edit Data 2.Save Data 3.Get Data
-  const [value, setValue] = useState("");
+export default function MyEditor() {
+  const [CurrentEditNote, setCurrentEditNote] = useContext(EditNoteContext);
+  // TODO: use mainRef.current.unprivilegedEditor.getContents() to get current editor delta content
+  // * : quill js accept plain text as value or delta object
+  // *DOC https://stackoverflow.com/questions/53809521/quill-js-how-to-use-settext-with-basic-formatting
+  // TODO: if note is different change isDifferentContext to true , default is false
+  const [EditorValue, setEditorValue] = useState("");
   useEffect(() => {
-    console.log(mainRef.current.unprivilegedEditor.getContents());
-  }, [value]);
+    // console.log(mainRef.current.unprivilegedEditor.getContents());
+    setEditorValue(CurrentEditNote?.paragraph);
+  }, [CurrentEditNote]);
   return (
     <div className="Edit-Note-Section">
       <ReactQuill
         theme="snow"
-        value={value}
-        onChange={setValue}
+        value={EditorValue}
+        onChange={setEditorValue}
         ref={mainRef}
       />
     </div>
