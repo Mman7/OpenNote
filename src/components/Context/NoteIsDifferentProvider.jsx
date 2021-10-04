4650458bc7af2e0e01464c04a54477ac80efd759
Mackenzie <youzai0427@gmail.com>
2021-10-04 18:01:20 +0800

    Add ➕ isDiffferent function

--- src/components/Context/NoteIsDifferentProvider.jsx
+++ src/components/Context/NoteIsDifferentProvider.jsx
@@ -1,4 +1,9 @@
 import React, { useState, createContext, useRef, createRef } from "react";
+import { isEqual } from "lodash";
+
+// if its different return true else return false
+export const isDifferentComparator = (object_1, object_2) =>
+  isEqual(object_1, object_2) ? false : true;
 
 export const isDifferentContext = createContext();
 
