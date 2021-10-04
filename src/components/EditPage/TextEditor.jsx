e12458b6bae9f805a4e145a35d81932b0416cae2
Mackenzie <youzai0427@gmail.com>
2021-10-04 18:56:13 +0800

    Update🔼 editor tracking note now

--- src/components/EditPage/TextEditor.jsx
+++ src/components/EditPage/TextEditor.jsx
@@ -1,22 +1,39 @@
 import React, { useState, createRef, useEffect, useContext } from "react";
-import { isDifferentContext } from "../Context/NoteIsDifferentProvider";
+import {
+  isDifferentContext,
+  isDifferentComparator,
+} from "../Context/NoteIsDifferentProvider";
 import ReactQuill from "react-quill";
 import { EditNoteContext } from "../Context/EditNoteContextProvider";
 import "react-quill/dist/quill.snow.css";
 
 let mainRef = createRef();
 
-export default function MyEditor() {
-  const [CurrentEditNote, setCurrentEditNote] = useContext(EditNoteContext);
-  // TODO: use mainRef.current.unprivilegedEditor.getContents() to get current editor delta content
-  // * : quill js accept plain text as value or delta object
-  // *DOC https://stackoverflow.com/questions/53809521/quill-js-how-to-use-settext-with-basic-formatting
-  // TODO: if note is different change isDifferentContext to true , default is false
+export default function MyEditor({
+  CurrentEditNote,
+  setCurrentEditNote,
+  setParagraph,
+  paragraphHandler,
+  UpdateGlobalContent,
+}) {
+  const [isDifferent, setisDifferent] = useContext(isDifferentContext);
   const [EditorValue, setEditorValue] = useState("");
+
   useEffect(() => {
-    // console.log(mainRef.current.unprivilegedEditor.getContents());
+    // if currentEditnote change editor change
     setEditorValue(CurrentEditNote?.paragraph);
   }, [CurrentEditNote]);
+
+  useEffect(() => {
+    // trancking save btn
+    const currentContent = CurrentEditNote;
+    const editorContent = mainRef.current.unprivilegedEditor.getContents();
+    setisDifferent(isDifferentComparator(editorContent, currentContent));
+
+    //paragraphHandler function is update content to global content
+    setParagraph(mainRef.current.unprivilegedEditor.getContents());
+  }, [EditorValue]);
+
   return (
     <div className="Edit-Note-Section">
       <ReactQuill
