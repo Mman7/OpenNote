29bfd1355fbc069814fcb1e61577dfd0e63f388a
Mackenzie <youzai0427@gmail.com>
2021-10-04 18:57:03 +0800

    Update 🔼 more readable code

--- src/components/EditPage/ViewNavBar.jsx
+++ src/components/EditPage/ViewNavBar.jsx
@@ -1,23 +1,37 @@
 import React, { useContext } from "react";
 import { ShowEditContext } from "../OpenNote";
 import { isDifferentContext } from "../Context/NoteIsDifferentProvider";
+import { EditNoteContext } from "../Context/EditNoteContextProvider";
+import { NewNote } from "../FireBaseApi";
 
 export default function ViewNavBar() {
   const [, setShowEdit] = useContext(ShowEditContext);
   const [isDifferent, setisDifferent] = useContext(isDifferentContext);
+  const [CurrentEditNote, setCurrentEditNote] = useContext(EditNoteContext);
 
-  const Back = () => {
+  const BackToNotePage = () => {
     setShowEdit((prev) => !prev);
+    //clean CurrentEditNote
+    setCurrentEditNote();
   };
 
-  const isDifferentHandle = () => (isDifferent ? "Save-Note-btn-show" : "");
+  const isDifferentHandler = () => (isDifferent ? "Save-Note-btn-show" : "");
+
+  const saveChangesHandler = () => {
+    console.log(CurrentEditNote);
+  };
 
   return (
     <div className="ViewNavBar">
-      <button onClick={Back}>
+      <button onClick={BackToNotePage}>
         <i className="fas fa-chevron-left"></i>
       </button>
-      <button className={`Save-Note-btn ${isDifferentHandle()}`}>✅</button>
+      <button
+        onClick={saveChangesHandler}
+        className={`Save-Note-btn ${isDifferentHandler()}`}
+      >
+        <i className="fas fa-check-square"></i>
+      </button>
     </div>
   );
 }
