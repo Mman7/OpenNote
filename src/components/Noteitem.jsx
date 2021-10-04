d64bdc462d0069ca7a562a154f0d0df75d83474f
Mackenzie <youzai0427@gmail.com>
2021-10-05 00:45:56 +0800

    Update🔼reactmarkdown is no longer for preview

--- src/components/Noteitem.jsx
+++ src/components/Noteitem.jsx
@@ -1,14 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect, createRef } from "react";
 import { ShowEditContext } from "./OpenNote";
 import { EditNoteContext } from "./Context/EditNoteContextProvider";
 import { NoteContext } from "./Context/NoteListContextProvider";
 import { NormalizeTime } from "./Logical-Javascript/NormalizeTime";
-import ReactMarkdown from "react-markdown";
+import ReactQuill from "react-quill";
+
+let mainRef = createRef();
 
 export default function Noteitem({ note }) {
   const [Notes, setNotes] = useContext(NoteContext);
   const [ShowEditPage, setShowEditPage] = useContext(ShowEditContext);
   const [CurrentEditNote, setCurrentEditNote] = useContext(EditNoteContext);
+
+  //FIXME check if bug is fix
+
+  const isDeltaData = () =>
+    note.paragraph[0] === "{" ? JSON.parse(note.paragraph) : note.paragraph;
 
   const EditNoteHandle = () => {
     setShowEditPage((prev) => !prev);
@@ -19,6 +26,10 @@ export default function Noteitem({ note }) {
     const EditThisNote = Notes.filter((note) => note.noteid === NoteId);
     setCurrentEditNote(EditThisNote[0]);
     EditNoteHandle();
+  };
+
+  const modules = {
+    toolbar: false,
   };
 
   return (
@@ -32,9 +43,15 @@ export default function Noteitem({ note }) {
       <h2 data-plaintext="true" className="Noteitem-time">
         Lastest Update: {NormalizeTime(note.createdAt)}
       </h2>
-      <ReactMarkdown className="Noteitem-paragraph">
-        {note.paragraph}
-      </ReactMarkdown>
+
+      <ReactQuill
+        className="Noteitem-paragraph"
+        theme="snow"
+        value={isDeltaData()}
+        ref={mainRef}
+        readOnly={true}
+        modules={modules}
+      />
     </div>
   );
 }
