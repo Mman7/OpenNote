a2ba590d5f05b9784018c7c2b15d2286c5a198ec
Mackenzie <youzai0427@gmail.com>
2021-10-05 17:41:04 +0800

    Update✅ Add note update checker

--- src/components/EditPage/ViewNavBar.jsx
+++ src/components/EditPage/ViewNavBar.jsx
@@ -1,24 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { ShowEditContext } from "../OpenNote";
 import { isDifferentContext } from "../Context/NoteIsDifferentProvider";
+import { NoteContext } from "../Context/NoteListContextProvider";
 import { EditNoteContext } from "../Context/EditNoteContextProvider";
-import { NewNote } from "../FireBaseApi";
+import { SaveNote_To_DataBase, getData_From_DataBase } from "../FireBaseApi";
+import { GlobalNoteContext } from "../Context/GlobalValueContext";
 
 export default function ViewNavBar() {
+  const [Notes, setNotes] = useContext(NoteContext);
   const [, setShowEdit] = useContext(ShowEditContext);
   const [isDifferent, setisDifferent] = useContext(isDifferentContext);
   const [CurrentEditNote, setCurrentEditNote] = useContext(EditNoteContext);
+  const [GlobalValueContext, setGlobalValueContext] =
+    useContext(GlobalNoteContext);
 
-  const BackToNotePage = () => {
+  const BackToNotePage = async () => {
     setShowEdit((prev) => !prev);
-    //clean CurrentEditNote
-    setCurrentEditNote();
+    //clean CurrentEditNote and GlobalValueContext
+    setCurrentEditNote(null);
+    setGlobalValueContext(null);
+    // update checker
+
+    const newNote = await getData_From_DataBase();
+    setNotes(() => newNote);
   };
 
   const isDifferentHandler = () => (isDifferent ? "Save-Note-btn-show" : "");
 
+  //TODO Save Note function
   const saveChangesHandler = () => {
-    console.log(CurrentEditNote);
+    SaveNote_To_DataBase(GlobalValueContext);
   };
 
   return (
