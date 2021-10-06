539d5ebf1ab90fa07520cb5e5d495157f5d023e5
Mackenzie <youzai0427@gmail.com>
2021-10-06 20:20:55 +0800

    Update✅ simplify code

--- src/components/Context/NoteIsDifferentProvider.jsx
+++ src/components/Context/NoteIsDifferentProvider.jsx
@@ -1,17 +1,19 @@
-import React, { useState, createContext, useRef, createRef } from "react";
+import React, { useState, createContext } from "react";
 import { isEqual } from "lodash";
-
-// if its different return true else return false
-export const isDifferentComparator = (object_1, object_2) =>
-  isEqual(object_1, object_2) ? false : true;
 
 export const isDifferentContext = createContext();
 
 export function NoteIsDifferentProvider({ children }) {
+  // if any different change to true
+  const isAnyDifferent = (object_1, object_2) =>
+    setisDifferent(isEqual(object_1, object_2) ? false : true);
+
   const [isDifferent, setisDifferent] = useState(false);
 
   return (
-    <isDifferentContext.Provider value={[isDifferent, setisDifferent]}>
+    <isDifferentContext.Provider
+      value={[isDifferent, setisDifferent, isAnyDifferent]}
+    >
       {children}
     </isDifferentContext.Provider>
   );
