ccd323e5a97c15ef2212c635836946b78f2fd393
Mackenzie <youzai0427@gmail.com>
2021-10-06 20:21:47 +0800

    Update✅ Add UpdateList Function

--- src/components/Context/NoteListContextProvider.jsx
+++ src/components/Context/NoteListContextProvider.jsx
@@ -6,13 +6,18 @@ export const NoteContext = createContext([]);
 export function NoteListContextProvider({ children }) {
   const [Notes, setNotes] = useState([]);
 
-  useEffect(async () => {
+  const UpdateList = async () => {
     const data = await getData_From_DataBase();
     setNotes(() => data);
+  };
+
+  useEffect(() => {
+    //intial notelist
+    UpdateList();
   }, []);
 
   return (
-    <NoteContext.Provider value={[Notes, setNotes]}>
+    <NoteContext.Provider value={[Notes, setNotes, UpdateList]}>
       {children}
     </NoteContext.Provider>
   );
