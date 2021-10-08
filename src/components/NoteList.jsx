cf23b84f3b1579def18f34442c3c51bf658327dd
Mackenzie <youzai0427@gmail.com>
2021-10-08 15:09:34 +0800

    Update✅ Add Search bar function

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,15 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { NoteContext } from "./Context/NoteListContextProvider";
 import Noteitem from "./Noteitem";
 
 export default function NoteList() {
-  const [Notes, setNotes] = useContext(NoteContext);
+  const [Notes, setNotes, , hadSearchBarValue] = useContext(NoteContext);
 
-  return (
-    <div className="NoteList">
-      {Notes.map((note) => (
-        <Noteitem key={note.noteid} note={note} />
-      ))}
-    </div>
-  );
+  const searchBarValueHandler = () => (hadSearchBarValue !== "" ? true : false);
+
+  // Notes.filter((note) => note.title.toLowerCase().includes(value))
+  // noteitem base on search bar value
+  const ULT = () =>
+    Notes.filter((note) =>
+      note.title.toLowerCase().includes(hadSearchBarValue)
+    ).map((note) => <Noteitem key={note.noteid} note={note} />);
+
+  return <div className="NoteList">{ULT()}</div>;
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,22 +1,31 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { ShowEditContext } from "./OpenNote";
 import { EditNoteContext } from "./Context/EditNoteContextProvider";
+import { FormControl } from "react-bootstrap";
+import { includes } from "lodash";
+import { NoteContext } from "./Context/NoteListContextProvider";
 
 export default function SearchBar() {
+  const [Notes, setNotes, , hadSearchBarValue, sethadSearchBarValue] =
+    useContext(NoteContext);
   const [Edit, setShowEdit, setSidebar] = useContext(ShowEditContext);
   const [CurrentEditNote, setCurrentEditNote] = useContext(EditNoteContext);
 
-  //TODO: New Note btn function
   const AddNote = () => {
     setShowEdit((prev) => !prev);
     setCurrentEditNote("");
     setSidebar(false);
   };
 
+  const SearchHandler = (e) => {
+    const value = e?.target.value;
+    sethadSearchBarValue(value);
+  };
+
   return (
     <div className="Searchbar">
       <i className="fas fa-search"></i>
-      <input type="text" />
+      <FormControl onChange={(e) => SearchHandler(e)}></FormControl>
       <button className="Add-btn" onClick={AddNote}>
         <i className="fas fa-plus-square"></i>
       </button>
