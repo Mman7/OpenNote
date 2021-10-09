b3fb8b1251c4fc23728b7942fb878ffd27468766
Mackenzie <youzai0427@gmail.com>
2021-10-09 20:33:52 +0800

    Update✅Add Popup when error

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,14 +1,27 @@
 import React, { createRef } from "react";
 import { Form, Button } from "react-bootstrap";
 import { createAccountWithEmail } from "./FireBaseApi";
+import PopUp from "./PopUp";
+
+import { getAuth } from "firebase/auth";
 
 const EmailRef = createRef();
 const PasswordRef = createRef();
 
 export default function SignInPage({ ShowSignUpPageHandler }) {
-  const createAccountHandler = () => {
-    createAccountWithEmail();
+  const [Pop] = PopUp();
+
+  const createAccountHandler = async (e) => {
+    try {
+      e.preventDefault();
+      const email = EmailRef.current.value;
+      const password = PasswordRef.current.value;
+      const isSignUp = await createAccountWithEmail(email, password);
+    } catch (err) {
+      Pop(err.code);
+    }
   };
+
   return (
     <div className="sign-in-page">
       <Form>
@@ -27,7 +40,11 @@ export default function SignInPage({ ShowSignUpPageHandler }) {
           />
         </Form.Group>
 
-        <Button variant="primary" type="submit">
+        <Button
+          onClick={(e) => createAccountHandler(e)}
+          variant="primary"
+          type="submit"
+        >
           Submit
         </Button>
         <button
