43cf7dd3575c29f2859cf476baf9eeb05b995619
MackenzLaptop <youzai0427@gmail.com>
2021-10-13 04:19:19 +0800

    Update✅ simplify popup

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,17 +1,20 @@
-import React from "react";
 import Swal from "sweetalert2";
 import "sweetalert2/src/sweetalert2.scss";
 
-export default function PopUp() {
-  const Pop = (message) => {
-    console.log(message);
-    Swal.fire({
-      title: message,
-      text: "",
-      icon: "error",
-      confirmButtonText: "Okay",
-    });
-  };
+export default function PopUp(message) {
+  Swal.fire({
+    title: message,
+    text: "",
+    icon: "error",
+    confirmButtonText: "Okay",
+  });
+}
 
-  return [Pop];
+export function PopCheck(message) {
+  Swal.fire({
+    title: message,
+    icon: "success",
+    showConfirmButton: false,
+    timer: 1500,
+  });
 }
