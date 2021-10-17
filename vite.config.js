426a615e415f1942f4767f024f6d74f193b2b506
Mackenzie <youzai0427@gmail.com>
2021-10-17 18:59:28 +0800

    update✅  new config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
-import replace from "@rollup/plugin-replace";
-import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("production"),
-      __buildDate__: () => JSON.stringify(new Date()),
-      __buildVersion: 15,
-    }),
-    commonjs(),
-    terser(),
-  ],
+  plugins: [reactRefresh()],
   define: {
     "process.env": process.env,
   },
